9b60ba94a0f67346f78a287711a84eb0f579a748
Viciot <vfuentesnoguera@gmail.com>
2021-08-01 18:51:38 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-
-import MainPage from './pages/MainPage/MainPage';
-import AllBeers from './pages/AllBeers/AllBeers'
-import SingleBeer from './pages/SingleBeer/SingleBeer'
-import RandomBeer from './pages/RandomBeer/RandomBeer'
-import NewBeer from './pages/NewBeer/NewBeer'
 
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import CountryList from './components/CountryList';
+import Navbar from './components/Navbar';
+import CountryDetails from './components/CountryDetails.js';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+	return (
+		<div className='App'>
+			<Navbar />
 
-    
-      <Switch>
-        <Route path="/" component={MainPage} exact />
-        <Route path="/all-beers" component={AllBeers} exact />
-        <Route path="/beers/:id" component={SingleBeer} exact />
-        <Route path="/random-beer" component={RandomBeer} exact />
-        <Route path="/new-beer" component={NewBeer} exact />
-      </Switch>
-    </div>
-  );
-}
+			<Switch>
+				<Route exact path='/' component={CountryList} />
+				<Route exact path='/details/:countryId' component={CountryDetails} />
+			</Switch>
+		</div>
+	);
+};
+
+/* <Route
+    exact
+    path="/details/:countryId"
+    render={props => (
+   <CountryDetails
+    {...props}
+    countryArr={this.state.countriesArray}
+/> */
 
 export default App;
